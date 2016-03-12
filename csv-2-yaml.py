63b60cc1b5d0d38ca85d68e19684d68857d44165
vagrant <vagrant@precise64.(none)>
2016-03-12 14:29:09 +0000

    initial version

---
+++ csv-2-yaml.py
@@ -0,0 +1,22 @@
+import yaml
+import csv
+
+bds = dict(
+    bds={}
+)
+
+csv_file = 'vlan-ids.csv'
+yml_file = 'group_vars/all/bds.yaml'
+
+with open(csv_file, 'rb') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=';')
+    for row in reader:
+        bd = 'bd_vlan' + row['VLAN']
+        bds['bds'][bd] = {}
+        bds['bds'][bd]['desc'] = str(row['Omschrijving']).replace(" ", "-")
+        bds['bds'][bd]['vlan_id'] = int(row['VLAN'])
+        bds['bds'][bd]['leafs'] = str(row['leafs'])
+        bds['bds'][bd]['ipg'] = str(row['interface_policy_group'])
+
+with open(yml_file,'w') as ymlfile:
+    ymlfile.write(yaml.dump(bds, default_flow_style=False))
